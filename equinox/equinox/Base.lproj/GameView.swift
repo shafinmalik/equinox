4dfeacd3e98a460ad808dd887aae5f96753df5f9
Shafin Malik <shafinmalik@Shafins-MacBook-Pro.local>
2023-10-22 00:25:14 -0400

    Render pipeline 0.1

--- equinox/equinox/Base.lproj/GameView.swift
+++ equinox/equinox/Base.lproj/GameView.swift
@@ -8,13 +8,21 @@
 import MetalKit
 
 class GameView: MTKView {
+    var commandQueue: MTLCommandQueue!
     required init(coder: NSCoder) {
         super.init(coder:coder)
         
+        self.device = MTLCreateSystemDefaultDevice()
+        self.clearColor = MTLClearColorMake(0.43, 0.73, 0.35, 1.0)
+        self.colorPixelFormat = .bgra8Unorm
+        
+        self.commandQueue = device?.makeCommandQueue()
     }
     
-    override func draw(_dirtyRect: NSRect) {
-        
+    override func draw(_ dirtyRect: NSRect) {
+        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
     }
     
 }
